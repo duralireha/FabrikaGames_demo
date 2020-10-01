413d74e156362395b087b4173b184c01ea5e0cbd
duralireha <duralireha@gmail.com>
2020-10-01 20:30:54 +0300

    Update CreateWood.cs

--- Assets/Scripts/CreateWood.cs
+++ Assets/Scripts/CreateWood.cs
@@ -10,25 +10,24 @@ public class CreateWood : MonoBehaviour {
 
 	public GameObject wood; //yaratmak istediğimiz nesne 
 	private float timer; //yaratma aralığı 
-	private float speed; 
+	private float positionMax, positionMin; //yaratma pozisyonu 
+	private float rangeXZ, rangeY; 
 
 	void Start() { 
-		speed = 10.0f; 
 		timer = 1.5f; //yaratma aralığının belirlenmesi 
+		rangeY = Random.Range(0.5f, 1.0f); //wood nesnesinin y scale değerinin rastgele belirlenmesi 
+		rangeXZ = Random.Range(0.3f, 1.0f); //wood nesnesinin x ve z scale değerlerinin aynı rastgele sayı olması 
+		positionMax = 2.0f - (rangeY / 2); //wood nesnesinin yaratılma pozisyonunun minimum değeri 
+		positionMin = -2.0f + (rangeY / 2); //wood nesnesinin yaratılma pozisyonunun maximum değeri  
+
         InvokeRepeating ("Create", timer, timer); //Wood fonksiyonunun belirlenen aralıklarla çağırılması 
     } //end Start 
-
-	void LateUpdate() {
-
-	}
-
+	
 	//Belirlediğimiz nesnenin yaratılması fonksiyonu 
-	public void Create() {
-		float rangeXZ = Random.Range(0.3f, 1.0f); 
-		GameObject newWood = Instantiate(wood, new Vector3(0.0f, 5.0f, 2.0f), Quaternion.Euler(0, 0, 90)); 
-		newWood.transform.localScale = new Vector3 (rangeXZ, Random.Range(0.5f, 1.0f), rangeXZ); 
-		newWood.GetComponent<Rigidbody>().angularVelocity = new Vector3 (-speed, 0.0f, 0.0f);  
-		newWood.tag = "Split"; 
+	public void Create() { 
+		GameObject newWood = Instantiate(wood, new Vector3(Random.Range(positionMin, positionMax), 5.0f, 2.0f), Quaternion.Euler(0, 0, 90)); //wood nesnesinin yaratılması 
+		newWood.transform.localScale = new Vector3 (rangeXZ, rangeY, rangeXZ); //yaratılan nesnenin rastgele scale edilmesi 
+		newWood.tag = "Split"; //yaratılan nesnenin SplitWood script'ine uygun şekilde etiketlenmesi 
 	} //end CreateWood 
 
 } //eof 
